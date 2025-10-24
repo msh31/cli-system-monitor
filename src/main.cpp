44b58c0d2aa7bd19c6bdc4b1de52dc4dc34430a1
msh31 <180540729+msh31@users.noreply.github.com>
2025-10-24 15:28:30 +0200

    feat: read json files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <fstream>
+
+#include <nlohmann/json.hpp>
 #include "ResourceMonitor.hpp"
+
+using json = nlohmann::json;
 
 int main() {
     std::cout << "hi\n";
+    std::ifstream f("config.json");
+    json data = json::parse(f);
+    std::cout << data << "\n";
     return 0;
 }
