43a388c80eb74cfb58961e161f1d67ee650a3058
msh31 <180540729+msh31@users.noreply.github.com>
2025-10-24 15:33:49 +0200

    feat: nice display of json entries

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,18 @@
 using json = nlohmann::json;
 
 int main() {
-    std::cout << "hi\n";
     std::ifstream f("config.json");
     json data = json::parse(f);
-    std::cout << data << "\n";
+
+    int CPU_THRESHOLD = data["CPU_THRESHOLD"];
+    int CPU_MAXTEMP = data["CPU_MAXTEMP"];
+    int RAM_THRESHOLD = data["RAM_THRESHOLD"];
+    int PROCESS_THRESHOLD = data["PROCESS_THRESHOLD"];
+
+    std::cout << "CPU Threshold: " << CPU_THRESHOLD << "\n";
+    std::cout << "CPU Max temp: " << CPU_MAXTEMP << "\n\n";
+    std::cout << "RAM Threshold: " << RAM_THRESHOLD << "\n\n";
+    std::cout << "Process Threshold: " << PROCESS_THRESHOLD << "\n";
+
     return 0;
 }
