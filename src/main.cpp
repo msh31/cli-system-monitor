176c850addc2a5697c82d890d333859128c83f28
Marco <180540729+msh31@users.noreply.github.com>
2025-11-14 18:51:44 +0100

    improve: browser ram usage display
    - updates inline
    - fixed vivaldi,waterfox,librefox,opera vector items

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,10 @@ std::vector<std::string> browsersToCheck = { //non existing ones will just retur
     "firefox.exe",
     "msedge.exe",
     "brave.exe",
-    "vivaldi",
-    "waterfox",
-    "librefox",
-    "opera"
+    "vivaldi.exe",
+    "waterfox.exe",
+    "librefox.exe",
+    "opera.exe"
 };
 
 void signalHandler(int signal) {
@@ -38,15 +38,30 @@ int main() {
         return 1;
     }
 
+    for (int i = 0; i < browsersToCheck.size(); i++) {
+        std::cout << browsersToCheck[i] << "'s ram usage: 0MB\n";
+    }
+
     while (true) {
         //TODO: improve CPU & RAM precision
         resMon.runAnalysis();
 
+        // move cursor up N lines
+        std::cout << "\033[" << browsersToCheck.size() << "A";
+
         for (const auto& browser : browsersToCheck) {
-            size_t memory = resMon.getProcessRamUsage(browser);
+            size_t memory = 0;
+
+            try {
+                memory += resMon.getProcessRamUsage(browser);
+            }
+            catch (const ExceptionHandler& ex) {
+                std::cerr << "Error: " << ex.what() << "\n";
+            }
+
+            std::cout << "\33[2K";  // clear entire line
             std::cout << browser << "'s ram usage: " << memory << "MB\n";
         }
-        std::cout << "\n";
 
         std::this_thread::sleep_for(std::chrono::seconds(LOOP_INTERVAL));
         timeRan++;
